69c5caa57d66f2210cc031b6b31a02578eda1423
KOOTTUMMEL Arun <ako1524@students.fhv.at>
2026-03-01 13:00:37 +0100

    implement Http Post to assign medication to patient

--- Backend/Backend/Api/Controller/PatientController.cs
+++ Backend/Backend/Api/Controller/PatientController.cs
@@ -3,7 +3,24 @@ using Microsoft.AspNetCore.Mvc;
 namespace Backend.Api.Controller;
 
 [ApiController]
-[Route("api/Patient")]
+[Route("api/patients/")]
 public class PatientController :ControllerBase
 {
+    [HttpGet("{patientId:int}/medications")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public ActionResult<IEnumerable<Medication>> GetMedicationsById(int patientId)
+    {
+        return Ok();
+
+    }
+
+    [HttpPost("{patientId:int}/medications/{medicationId:int}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public ActionResult AssignNewMedicationToPatient(int patientId, int medicationId)
+    {
+        return NoContent();
+    }
+    
 }
