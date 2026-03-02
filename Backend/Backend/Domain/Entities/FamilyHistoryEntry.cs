5c19c3514b962386bfcbc9cee9d6d6b5bf9fb365
amir.oez <amir.oez@loacker.cc>
2026-03-02 13:24:42 +0100

    Creation of Entity FamilyHistoryEntry

---
+++ Backend/Backend/Domain/Entities/FamilyHistoryEntry.cs
@@ -0,0 +1,14 @@
+﻿using Backend.Domain.Interfaces;
+
+
+namespace Backend.Domain.Entities
+
+{
+    public class FamilyHistoryEntry : IEntity
+    {
+        public int Id { get; set; }
+        
+        
+    }
+    
+}
