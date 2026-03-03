e68e8e5acff5a134e111574395e33197157e40d7
MBilmom <amir.oez@loacker.cc>
2026-03-03 17:19:09 +0100

    Add generic IRepository and IFamilyHistoryRepository interfaces

---
+++ Backend/Backend/Application/Repositories/IFamilyHistoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Backend.Domain.Entities;
+using Backend.Domain.Interfaces;
+
+namespace Backend.Application.Repositories
+{
+    public interface IFamilyHistoryRepository : IRepository<FamilyHistoryEntry>
+    {
+        Task<List<FamilyHistoryEntry>> FindByPatientIdAsync(int patientId);
+    }
+}
