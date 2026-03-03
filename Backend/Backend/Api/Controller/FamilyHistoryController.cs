e83a81d5bbf80611cbaf7658112f7b2b45219024
amir.oez <amir.oez@loacker.cc>
2026-03-03 11:50:35 +0100

    Creation FamilyHistoryController

---
+++ Backend/Backend/Api/Controller/FamilyHistoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Api.Controller
+{
+    [ApiController]
+    [Route("api/family-history-entries")]
+    public class FamilyHistoryController : ControllerBase
+    {
+        
+    }
+}
