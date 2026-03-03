8c1bb0a5e5d4f61acd665c8d90245c9475148164
amir.oez <amir.oez@loacker.cc>
2026-03-03 10:17:00 +0100

    adding properties for FamilyHistoryEntry

--- Backend/Backend/Domain/Entities/FamilyHistoryEntry.cs
+++ Backend/Backend/Domain/Entities/FamilyHistoryEntry.cs
@@ -2,13 +2,15 @@
 
 
 namespace Backend.Domain.Entities
-
 {
-    public class FamilyHistoryEntry : IEntity
+    public class FamilyHistoryEntry : IEntity //Familienanamnese
     {
         public int Id { get; set; }
-        
-        
+        public int PatientId { get; set; } //foreign key to patient, so no need for an attribute like name
+        public string Relative { get; set; } = string.Empty;
+
+        public string Diagnosis { get; set; } = string.Empty;
+
+        public string? Comment { get; set; } = string.Empty; //optional
     }
-    
 }
