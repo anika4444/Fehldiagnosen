e5a62cd3c1a37c94cbbf2eb81744b09e3a34c19f
MBilmom <amir.oez@loacker.cc>
2026-03-09 12:52:49 +0100

    Creation of first Get Methods by PatientId and HistoryEntryId

--- Backend/Backend/Api/Controller/FamilyHistoryController.cs
+++ Backend/Backend/Api/Controller/FamilyHistoryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Backend.Application.Repositories;
+using Backend.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Api.Controller
 {
@@ -6,6 +8,42 @@ namespace Backend.Api.Controller
     [Route("api/family-history-entries")]
     public class FamilyHistoryController : ControllerBase
     {
-        
+        private readonly IFamilyHistoryRepository _repository;
+
+        public FamilyHistoryController(IFamilyHistoryRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("family-history-entries/{historyEntryId:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<FamilyHistoryEntry>>> GetEntriesByHistoryEntryId(int historyEntryId)
+        {
+            var entries = await _repository.FindByIdAsync(historyEntryId);
+
+            if (entries == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(entries);
+        }
+
+
+        [HttpGet("patients/{patientId:int}/family-history-entries")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<FamilyHistoryEntry>> GetEntriesByPatientId(int patientId)
+        {
+            var entry = await _repository.FindByPatientIdAsync(patientId);
+
+            if (entry == null || !entry.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(entry);
+        }
     }
 }
