092498693cf92254107ee532e44d565a3d5a781f
Tronicsoul <arunwillsuchten@gmail.com>
2026-03-22 16:10:02 +0100

    Adding medication Repo

--- Backend/Backend/Domain/Entities/Medication.cs
+++ Backend/Backend/Domain/Entities/Medication.cs
@@ -8,14 +8,14 @@ namespace Backend.Domain.Entities
         public int Id { get; set; }
         public int PatientId { get; set; }
         public required string Name { get; set; }
-        public string dosage { get; set; }
-        public string intakeFrequency { get; set; }
+        public string? dosage { get; set; }
+        public string? intakeFrequency { get; set; }
         public DateOnly intakeStartDate { get; set; }
         public int DurationInDays { get; set; }
 
         public DateOnly EndDate =>
             intakeStartDate.AddDays(DurationInDays);
-        public string indication { get; set; }
-        public string issuedBy { get; set; }
+        public string? indication { get; set; }
+        public string? issuedBy { get; set; }
     }
 }
---
+++ Backend/Backend/Infrastructure/Repositories/MySqlMedicationRepository.cs
@@ -0,0 +1,48 @@
+﻿using Backend.Application.Repositories;
+using Backend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Infrastructure.Repositories
+{
+    public class MySqlMedicationRepository : IMedicationRepository
+    {
+        private readonly MySqlDbContext _context;
+        public MySqlMedicationRepository(MySqlDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Medication> AddAsync(Medication entity)
+        {
+
+            await _context.Medications.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<Medication> DeleteAsync(Medication entity)
+        {
+            _context.Medications.Remove(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<List<Medication>> FindAllAsync()
+        {
+            return await _context.Medications.ToListAsync();
+            
+        }
+
+        public async Task<Medication?> FindByIdAsync(int id)
+        {
+            Medication? medication = await _context.Medications.FindAsync(id);
+            return medication;
+        }
+
+        public async Task<Medication> UpdateAsync(Medication entity)
+        {
+            _context.Medications.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
