773fbd789f2dd94caf02d6a5df4b6163393da6df
Anika <schneider.anika@lampertmail.at>
2026-04-13 19:49:12 +0200

    add Health Tip for day

---
+++ Backend/Backend/Api/Controller/HealthTipController.cs
@@ -0,0 +1,32 @@
+﻿using Backend.Application.Services.HealthTipService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Api.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthTipController : BaseApiController
+    {
+        private readonly IHealthTipService _healthTipService;
+
+        public HealthTipController(IHealthTipService healthTipService)
+        {
+            _healthTipService = healthTipService;
+        }
+
+        [HttpGet("today")]
+        public IActionResult GetTodayHealthTip()
+        {
+            var result = _healthTipService.GetTodayHealthTipAsync();
+            if (result.IsSuccess)
+            {
+                return Ok(new
+                {
+                    Tip = result.Data
+                });
+            }
+
+            return BadRequest();
+        }
+    }
+}
---
+++ Backend/Backend/Application/Services/HealthTipService/IHealthTipService.cs
@@ -0,0 +1,9 @@
+﻿using Backend.Application.Common.Results;
+
+namespace Backend.Application.Services.HealthTipService
+{
+    public interface IHealthTipService
+    {
+        ServiceResult<string> GetTodayHealthTipAsync();
+    }
+}
